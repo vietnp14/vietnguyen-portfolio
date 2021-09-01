8cbdcb456bfbf68603873b4146e26a4d0480bdef
vietnp14 <vietnp14@gmail.com>
2021-09-01 10:43:28 +0700

    second initiation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Portfolio",
+    siteUrl: "https://www.phuongviet.info",
+    title: "Phuong Viet Portfolio",
   },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
 };
